0de38d77f95b30d954dbc2f17f3d2a179978d541
Quyln <kayq9999@gmail.com>
2023-11-19 14:41:37 +0700

    update generate pass

--- src/User/user.service.ts
+++ src/User/user.service.ts
@@ -107,7 +107,7 @@ export class UserService {
 
   generatePassword(): string {
     const characters =
-      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+      'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNOPQRSTUVWXYZ0123456789';
     let password = '';
     for (let i = 0; i < 6; i++) {
       const randomIndex = Math.floor(Math.random() * characters.length);
