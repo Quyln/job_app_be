c493117b71fb7113ab4dfbad9733d9348ef726a2
Quyln <kayq9999@gmail.com>
2023-12-21 15:14:59 +0700

    up

--- src/User/component/filter.user.dto.ts
+++ src/User/component/filter.user.dto.ts
@@ -1,6 +1,6 @@
 export class filterUserDto {
     id : string;
-    fullname? : string;
-    companyname? : string;
-    avatar? : string;
+    fullname : string;
+    companyname : string;
+    avatar : string;
 }
