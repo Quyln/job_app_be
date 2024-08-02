c6d8366ac94cd4d520cd1cc379921f7dc8544518
Quyln <kayq9999@gmail.com>
2024-08-02 14:01:53 +0700

    fix

--- src/LCCOM/lccom.entity.ts
+++ src/LCCOM/lccom.entity.ts
@@ -3,7 +3,7 @@ import { Column, Entity,PrimaryColumn } from "typeorm";
 @Entity()
 export class LCCOM {
     @PrimaryColumn()
-    companytag: String;
+    companytag: string;
     @Column()
     companyname: string;
     @Column()
