90c617276de8d07ee7e62b6889284ae12d011f81
Quyln <kayq9999@gmail.com>
2024-08-06 00:35:00 +0700

    update

--- src/User/component/update.user.dto.ts
+++ src/User/component/update.user.dto.ts
@@ -1,13 +1,8 @@
 export class updateUserDto {
          id : string;
-        email? : string;
         password : string;
         newpassword? :string;
-        fullname? : string;
-        companyname? : string;
-        companytag? : string;
         avatar? : string;
-        phone? : string;
         longitude? : string;
         latitude? : string;
      }
--- src/User/user.service.ts
+++ src/User/user.service.ts
@@ -21,6 +21,7 @@ export class UserService {
     const oldUserData = await this.userRepository.findOne({
       where: {
         id: id,
+        password : body.password
       },
     });
     
