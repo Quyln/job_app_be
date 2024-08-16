0e68e3a43af63b4e449a09d07dee78c78fad196b
Quyln <kayq9999@gmail.com>
2024-08-16 23:56:11 +0700

    update lccom

--- src/LCCOM/lccom.entity.ts
+++ src/LCCOM/lccom.entity.ts
@@ -3,11 +3,13 @@ import { Column, Entity,PrimaryColumn } from "typeorm";
 @Entity()
 export class LCCOM {
     @PrimaryColumn()
-    companytag: string;
+    id: string;
     @Column()
     companyname: string;
     @Column()
     branchname?: string;
+    @Column()
+    companytag: string;
     @Column()
     longitude?: string;
     @Column()
