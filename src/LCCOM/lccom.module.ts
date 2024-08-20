2ca0e5e28fccfb92fd84728472d90036cd6c12f1
Quyln <kayq9999@gmail.com>
2024-08-21 03:43:38 +0700

    fix

--- src/LCCOM/lccom.module.ts
+++ src/LCCOM/lccom.module.ts
@@ -5,11 +5,13 @@ import { MailModule } from "src/mail/mail.module";
 import { LCCOMController } from "./lccom.controller";
 import { LCCOMService } from "./lccom.service";
 import { User } from "src/User/user.entity";
+import { UserService } from "src/User/user.service";
+import { UserModule } from "src/User/user.module";
 
 @Module({
-imports : [TypeOrmModule.forFeature([LCCOM, User]), MailModule],
+imports : [TypeOrmModule.forFeature([LCCOM, User]), MailModule, UserModule],
 controllers: [LCCOMController],
-providers: [LCCOMService],
-exports: [LCCOMService]
+providers: [LCCOMService,UserService],
+exports: [LCCOMService,UserService]
 })
 export class LCCOMModule {}
